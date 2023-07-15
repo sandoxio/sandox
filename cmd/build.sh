f27925ba62dff96fb8eb86428584fab665305eef
xman <xman>
2023-07-15 21:56:02 +0300

    dist folder autocreate

--- cmd/build.sh
+++ cmd/build.sh
@@ -3,6 +3,7 @@
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 DIR="$(realpath "${DIR}/../")"
 
+mkdir -p "${DIR}/dist"
 cp "${DIR}/src/index.html" "${DIR}/dist/index.html"
 touch "${DIR}/dist/worker-javascript.js"
 
